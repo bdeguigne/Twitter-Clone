13e87579df3181f6748454fe44d40a2bb2f56c5c
bdeguigne <brice.deguigne@epitech.eu>
2020-08-28 18:41:37 +0400

    React setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+
+      {/* Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
